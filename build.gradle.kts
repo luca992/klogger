886676b1fff836b89c137a7cce8d1022621f9a8f
Luca Spinazzola <mspinluca@gmail.com>
2020-06-04 18:43:35 -0700

    update to kotlin 1.4-M2 (Local only)

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,11 +6,20 @@ buildscript {
         google()
         maven { url = uri("https://dl.bintray.com/korlibs/korlibs/") }
         maven { url = uri("https://plugins.gradle.org/m2/") }
+        maven { url = uri("https://dl.bintray.com/kotlin/kotlin-eap") }
         //maven { url = uri("https://dl.bintray.com/kotlin/kotlin-dev") }
     }
 
     dependencies {
-        classpath("com.soywiz.korlibs:easy-kotlin-mpp-gradle-plugin:0.8.1") // Kotlin 1.3.71: https://github.com/korlibs/easy-kotlin-mpp-gradle-plugin
+        classpath("com.soywiz.korlibs:easy-kotlin-mpp-gradle-plugin:0.8.6-1.4-M2") // Kotlin 1.3.71: https://github.com/korlibs/easy-kotlin-mpp-gradle-plugin
+    }
+}
+
+allprojects {
+    repositories {
+        mavenLocal()
+        jcenter()
+        maven { url = uri("https://dl.bintray.com/kotlin/kotlin-eap") }
     }
 }
 
